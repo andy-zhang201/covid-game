3248179559b50b5ad98c9cab2538d777d785cc26
davrine <daviddantelemme@gmail.com>
2020-05-23 22:45:31 -0400

    fixed moving bug, fixed rough spacing and fixed game timer

--- src.py
+++ src.py
@@ -4,16 +4,16 @@ pygame.init()
 win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("First Game")
 
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 5
+x = 0
+y = 0
+width = 50
+height = 50
+vel = 1
 
 run = True
 
 while run:
-    pygame.time.delay(100) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
+    pygame.time.delay(7) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
 
     for event in pygame.event.get():  # This will loop through a list of any keyboard or mouse events.
         if event.type == pygame.QUIT: # Checks if the red button in the corner of the window is clicked
@@ -21,10 +21,10 @@ while run:
             
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_RIGHT] and x >vel:
-        x+= vel
-    if keys[pygame.K_LEFT] and x < 500-width:
-        x-=vel
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+        x += vel
     
     win.fill((0,0,0))
     pygame.draw.rect(win, (255,0,0), (x, y, width, height))  #This takes: window/surface, color, rect
